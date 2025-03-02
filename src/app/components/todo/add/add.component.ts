2251042ebf9f6d56ce07a9c86458393720e92ba9
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-02 19:09:27 +0530

    feat : Implemented the UI for the Todo Add page.

--- src/app/components/todo/add/add.component.ts
+++ src/app/components/todo/add/add.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css'],
 })
-export class AddComponent {}
+export class AddComponent {
+  // This is the user input stored here for the component
+  taskInput = {
+    id: '',
+    description: '',
+    isCompleted: false,
+  };
+
+  // This function is invoked if the submit / edit button is clicked
+  onSubmitClick() {
+    console.log(this.taskInput);
+  }
+}
