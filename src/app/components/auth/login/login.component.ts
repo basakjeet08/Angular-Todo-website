e975ba6b07b9472af33d186e6cc13f719da15255
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-02 15:08:18 +0530

    feat : Created the HTML Template for the Login Page.

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -5,4 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  // This is the input from the user
+  userInput = {
+    username: '',
+    password: '',
+  };
+
+  // This function is invoked when the user clicks on the login button
+  onLoginClick() {
+    alert(this.userInput.username + ' ' + this.userInput.password);
+  }
+
+  // This function is invoked when the user clicks on the go to register page button
+  onGoToRegisterClick() {
+    alert('Go To Register Page Click');
+  }
+}
